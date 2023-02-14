e074c7daa610d6286058aa62eeaa81e02c0cf84c
judibec <juanchopolo6954@gmail.com>
2023-02-13 23:41:17 -0500

    h2 configurado

---
+++ src/main/java/com/persistence/h2/demo/PersistenciaEnMemoriaConH2Application.java
@@ -0,0 +1,13 @@
+package com.persistence.h2.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersistenciaEnMemoriaConH2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersistenciaEnMemoriaConH2Application.class, args);
+	}
+
+}
